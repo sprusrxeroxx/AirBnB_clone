79863fa885aa9d94a993d2027c7d2e6f0cf74f93
ntokozo <ntokozovilakazi36@gmail.com>
2024-03-06 11:06:50 -0800

    console.py main implementation

--- console.py
+++ console.py
@@ -1,11 +1,29 @@
+#!/usr/bin/python3
+
 import cmd
 
-class HelloWorld(cmd.Cmd):
-    def do_greet(self, line):
-        print("hello", line)
+class HBNBCommand(cmd.Cmd):
+    prompt = '(hbnb)'
 
-    def do_EOF(self, line):
+    """Quit command to exit the program"""
+    def do_quit(self, line):
         return True
 
+    def help_quit(self):
+	    print("\nQuit command to exit the program\n")
+
+    def do_EOF(self, line):
+        print("\n")
+        return True
+
+    def help_EOF(self):
+	    print("\nEOF signal to quit the program\n")
+
 if __name__ == '__main__':
-    HelloWorld().cmdloop()
+    import sys
+
+    if len(sys.argv) > 1:
+        #Non Interactive mode
+        HBNBCommand().onecmd(' '.join(sys.argv[1:]))
+    else:
+        HBNBCommand().cmdloop()
