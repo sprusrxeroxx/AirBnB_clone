1df0f1d5e8d041e610706a967674d4a547c5b539
sprusrxeroxx <ntokozovilakazi36@gmail.com>
2024-03-10 09:27:44 +0200

    persistence establishment

---
+++ models/__init__.py
@@ -0,0 +1,6 @@
+from models.engine.file_storage import FileStorage
+
+# create an instance of Filestorage with the variable 'storage'
+storage = FileStorage()
+# call the reload() method on this instance
+storage.reload()
